8313312bc7a2f5c6247d5c0ca2e39b32e2c22273
Anupurba Mitra <mitraanupurba99@gmail.com>
2025-02-04 16:40:46 +0000

    new file added for hierarchical exceptions

---
+++ src/Ch02-Diving Deeper into Exceptions/02_03/begin/hierarchical_exceptions.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+enum ErrorType
+{
+  None,
+  Disk,
+  Permission,
+  FileIO
+};
+
+class DiskAccessException : public std::runtime_error
+{
+public:
+  DiskAccessException(const std::string &msg) : std::runtime_error(msg) {};
+};
+
+class FilePermissionException : public std::runtime_error
+{
+public:
+  FilePermissionException(const std::string &msg) : std::runtime_error(msg) {};
+};
+
+class FileIOException : public std::runtime_error
+{
+public:
+  FileIOException(const std::string &msg) : std::runtime_error(msg) {};
+};
+
+void triggerException(ErrorType error)
+{
+  switch (error)
+  {
+  case ErrorType::Disk:
+    throw DiskAccessException("DiskAccessException");
+  case ErrorType::Permission:
+    throw FilePermissionException("FilePermissionException");
+  case ErrorType::FileIO:
+    throw FileIOException("FileIOException");
+  default:
+    break; // No exception thrown
+  }
+}
+
+int main()
+{
+  try
+  {
+    triggerException(ErrorType::FileIO);
+  }
+  catch (const DiskAccessException &e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+  catch (const FilePermissionException &e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+  catch (const FileIOException &e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+  catch (...)
+  {
+    std::cerr << "An unknown exception occurred." << std::endl;
+  }
+  return 0;
+}
